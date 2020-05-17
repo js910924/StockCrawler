261379f2724fa1540ad91fa645ba6e017f99c7b7
Jason Chen <jason.chen@titansoft.com.sg>
2020-05-17 14:04:05 +0800

    get JSON stock information from TWSE

--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"fmt"
 	"flag"
+	"net/http"
+	"log"
+	"io/ioutil"
+	"strings"
 )
 
 var (
@@ -17,4 +21,23 @@ func main() {
 	flag.Parse()
 
 	fmt.Println("Get stock symbol: " + stockSymbol)
+	GetStockPicFromYahoo(stockSymbol)
+}
+
+func GetStockPicFromYahoo(stockSymbol string) {
+	url := "https://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch=tse_{stockSymbol}.tw&json=1&delay=0&_=1552123547443"
+	url = strings.Replace(url, "{stockSymbol}", stockSymbol, 1)
+	response, err := http.Get(url + stockSymbol)
+	if err != nil {
+		log.Fatalln("No such stock symbol: " + stockSymbol)
+	}
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatalln("Read response body error")
+	}
+
+	defer response.Body.Close()
+
+	fmt.Printf("%s\n", body)
 }
