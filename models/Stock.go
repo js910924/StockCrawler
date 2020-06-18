55dbbdba55317eba6c3f348c0cdc1722046984e9
js910924 <js910924@gmail.com>
2020-06-18 08:15:39 +0800

    move Stock struct to models/Stock.go

---
+++ models/Stock.go
@@ -0,0 +1,41 @@
+package models
+
+type Stock struct {
+	// A                        string `json:"a"`
+	// B                        string `json:"b"`
+	StockSymbol              string `json:"c"`
+	StockName                string `json:"n"`
+	CurrencyTransactionPrice string `json:"z"`
+	CurrentVolumn            string `json:"tv"`
+	OpeningPrice             string `json:"o"`
+	HighestPrice             string `json:"h"`
+	YesturdayClosePrice      string `json:"y"`
+	LowestPrice              string `json:"l"`
+	// Ch                       string `json:"ch"`
+	// D                        string `json:"d"`
+	// Ex                       string `json:"ex"`
+	// F                        string `json:"f"`
+	// Fv                       string `json:"fv"`
+	// G                        string `json:"g"`
+	// I                        string `json:"i"`
+	// IP                       string `json:"ip"`
+	// It                       string `json:"it"`
+	// Mt                       string `json:"mt"`
+	// Nf                       string `json:"nf"`
+	// Oa                       string `json:"oa"`
+	// Ob                       string `json:"ob"`
+	// Ot                       string `json:"ot"`
+	// Ov                       string `json:"ov"`
+	// Oz                       string `json:"oz"`
+	// P                        string `json:"p"`
+	// Ps                       string `json:"ps"`
+	// Pz                       string `json:"pz"`
+	// S                        string `json:"s"`
+	// T                        string `json:"t"`
+	// Tk0                      string `json:"tk0"`
+	// Tk1                      string `json:"tk1"`
+	// Tlong                    string `json:"tlong"`
+	// Ts                       string `json:"ts"`
+	// U                        string `json:"u"`
+	// W                        string `json:"w"`
+}
