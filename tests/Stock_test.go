79021b3bf114b23b055caedcb6b67891ea29d236
js910924 <js910924@gmail.com>
2020-06-21 11:05:32 +0800

    refactor test

--- tests/Stock_test.go
+++ tests/Stock_test.go
@@ -2,22 +2,11 @@ package testing
 
 import (
 	. "StockCrawler/models"
-	"strings"
 	"testing"
 )
 
 func TestWhenStockShowForm(t *testing.T) {
-	stock := Stock{
-		StockSymbol:              "2618",
-		StockName:                "長榮航",
-		CurrencyTransactionPrice: "12",
-		CurrentVolumn:            "2000",
-		OpeningPrice:             "12",
-		HighestPrice:             "12",
-		YesturdayClosePrice:      "12",
-		LowestPrice:              "12",
-	}
-
+	stock := givenStock()
 	actual := stock.ShowForm()
 	expected := `StockSymbol: 2618
 StockName: 長榮航
@@ -30,7 +19,20 @@ LowestPrice: 12
 Percent: 0.000%
 `
 
-	if strings.Compare(actual, expected) != 0 {
-		t.Errorf("\n%s is not equal\n%s", stock.ShowForm(), expected)
+	if actual != expected {
+		t.Errorf("\n%s is not equal\n%s", actual, expected)
+	}
+}
+
+func givenStock() Stock {
+	return Stock{
+		StockSymbol:              "2618",
+		StockName:                "長榮航",
+		CurrencyTransactionPrice: "12",
+		CurrentVolumn:            "2000",
+		OpeningPrice:             "12",
+		HighestPrice:             "12",
+		YesturdayClosePrice:      "12",
+		LowestPrice:              "12",
 	}
 }
