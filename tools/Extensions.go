add34bbe0e0ef4bcdec37c2efd493ae6a190de69
js910924 <js910924@gmail.com>
2020-06-22 21:23:57 +0800

    add GetFavoriteStockList method to load stocks

--- tools/Extensions.go
+++ tools/Extensions.go
@@ -2,10 +2,33 @@ package tools
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 )
 
 func GetFavoriteStockList() {
-	fmt.Println(`
-		Listssssssssss
-	`)
+	if err := recover(); err != nil {
+	}
+	defer CreateFile("favoriteList.txt")
+
+	file, err := os.Open("favoriteList.txt")
+	if err != nil {
+		log.Panicln("Can't find favoriteList.txt file")
+	}
+
+	defer file.Close()
+
+	b, err := ioutil.ReadAll(file)
+	fmt.Println(b)
+}
+
+func CreateFile(fileName string) {
+	file, err := os.Create(fileName)
+	if err != nil {
+		log.Panicln("Create file failed")
+	}
+
+	defer file.Close()
+	log.Printf("Create %s file successfully", fileName)
 }
